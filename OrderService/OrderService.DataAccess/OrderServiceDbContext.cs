1a20792d788cfdf64c4a89da9be3ff30bbaad312
airat <airat.hb@mail.ru>
2024-06-13 17:16:39 +0300

    add dbcontext

---
+++ OrderService/OrderService.DataAccess/OrderServiceDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.DataAccess.Entities;
+
+namespace OrderService.DataAccess
+{
+    public class OrderServiceDbContext : DbContext
+    {
+        public OrderServiceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TaskConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+
+        public DbSet<CurrentOrderEntity> CurrentOrders { get; set; }
+        public DbSet<LastOrderEnity> LastOrders { get; set; }
+        public DbSet<CanceledOrderEntity> CanceledOrders { get; set; }
+
+    }
+}
