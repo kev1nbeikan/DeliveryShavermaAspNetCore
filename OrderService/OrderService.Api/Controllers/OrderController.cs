7551b93bedcc8e202354f30069ff56b0415a30bf
airat <airat.hb@mail.ru>
2024-06-13 15:04:27 +0300

    make architecture

---
+++ OrderService/OrderService.Api/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace OrderService.Api.Controllers;
+
+[ApiController]
+[Route("orders")]
+public class OrderController: ControllerBase
+{
+    /*[HttpGet("get")]
+    public async Task<string> Get()
+    {
+        return "Asd";
+    }
+    
+    [HttpPut("get")]
+    public async Task<string> Get()
+    {
+        return "Asd";
+    }
+    
+    [HttpGet("get")]
+    public async Task<string> GetId()
+    {
+        return "Asd";
+    }*/
+}
