7f60b7b1af2dde73cff24c1c334831eb55cff63e
airat <airat.hb@mail.ru>
2024-06-15 19:42:56 +0300

    add currentOrder model

---
+++ OrderService/OrderService.Domain/Models/CurrentOrder.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace OrderService.Domain.Models;
+
+public class CurrentOrder : OrderBase
+{
+    private CurrentOrder(Guid id, Guid clientId, Guid courierId, Guid storeId,
+        JObject basket, int price, string comment, string clientAddress,
+        string courierNumber, string clientNumber, TimeSpan cookingTime,
+        TimeSpan deliveryTime, StatusCode status)
+        : base(id, clientId, courierId, storeId,
+        basket, price, comment,
+        clientAddress, courierNumber, clientNumber, cookingTime, deliveryTime)
+    {
+        Status = status;
+    }
+
+    public StatusCode Status { get; }
+
+    public static (CurrentOrder Order, string Error) Create(
+        Guid id, Guid clientId, Guid courierId, Guid storeId,
+        JObject basket, int price, string comment,
+        string clientAddress, string courierNumber, string clientNumber,
+        TimeSpan cookingTime, TimeSpan deliveryTime, StatusCode status)
+    {
+        string errorString = Check(id, clientId, courierId, storeId, basket,
+            price, comment, clientAddress, courierNumber, clientNumber, cookingTime,
+            deliveryTime);
+
+        var order = new CurrentOrder(id, clientId, courierId, storeId, basket, 
+            price, comment, clientAddress, courierNumber, clientNumber, cookingTime,
+            deliveryTime, status);
+
+        return (order, errorString);
+    }
+}
