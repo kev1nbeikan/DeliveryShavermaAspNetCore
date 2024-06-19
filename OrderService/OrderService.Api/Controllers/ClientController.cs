8909023768aae09c9dad3e169a4aa59ff83f6cfd
airat <airat.hb@mail.ru>
2024-06-19 23:31:22 +0300

    create base controller

---
+++ OrderService/OrderService.Api/Controllers/ClientController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Api.Contracts.Client;
+using OrderService.Api.Extensions;
+using OrderService.Domain.Abstractions;
+using OrderService.Domain.Models;
+using OrderService.Domain.Models.Code;
+
+namespace OrderService.Api.Controllers;
+
+[ApiController]
+[Route("orders/client")]
+public class ClientController(IOrderApplicationService orderApplicationService) : ControllerBase
+{
+    private readonly IOrderApplicationService _orderApplicationService = orderApplicationService;
+
+    [HttpGet("current")]
+    public async Task<ActionResult<List<ClientGetCurrent>>> GetCurrent(int filter)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+        
+        var orders = await _orderApplicationService.GetCurrentOrders(role, userId);
+        if (orders.Count == 0)
+            return NoContent();
+        var response = orders.Select(b => 
+            new ClientGetCurrent(b.Id, b.StoreId, b.Status, b.Basket, b.Price, b.Comment,
+                b.ClientAddress, b.CourierNumber, b.ClientNumber, b.Cheque));
+        return Ok(response);
+    }
+    
+    [HttpGet("last")]
+    public async Task<ActionResult<List<ClientGetLast>>> GetLast(int filter)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+        
+        var orders = await _orderApplicationService.GetLastOrders(role, userId);
+        if (orders.Count == 0)
+            return NoContent();
+        var response = orders.Select(b => 
+            new ClientGetLast(b.Id, b.StoreId, b.Basket, b.Price, b.Comment,
+                b.ClientAddress, b.ClientNumber, b.OrderDate, b.DeliveryDate, b.Cheque));
+        return Ok(response);
+    }
+    
+    [HttpGet("canceled")]
+    public async Task<ActionResult<List<ClientGetCanceled>>> GetCanceled(int filter)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+        
+        var orders = await _orderApplicationService.GetCanceledOrders(role, userId);
+        if (orders.Count == 0)
+            return NoContent();
+        var response = orders.Select(b => 
+            new ClientGetCanceled(b.Id, b.StoreId ,b.Basket, b.Price, b.Comment,
+                b.ClientAddress, b.ClientNumber, b.OrderDate, b.Cheque, b.LastStatus, b.ReasonOfCanceled));
+        return Ok(response);
+    }
+}
--- OrderService/OrderService.Api/Controllers/OrderController.cs
+++ OrderService/OrderService.Api/Controllers/OrderController.cs
@@ -1,28 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http.HttpResults;
-using System.Net;
+﻿namespace OrderService.Api.Controllers;
 
-namespace OrderService.Api.Controllers;
-
-[ApiController]
-[Route("orders")]
-public class OrderController: ControllerBase
+public class OrderController
 {
-    /*[HttpGet("get")]
-    public async Task<string> Get()
-    {
-        return "Asd";
-    }
     
-    [HttpPut("get")]
-    public async Task<string> Get()
-    {
-        return "Asd";
-    }
-    
-    [HttpGet("get")]
-    public async Task<string> GetId()
-    {
-        return "Asd";
-    }*/
 }
