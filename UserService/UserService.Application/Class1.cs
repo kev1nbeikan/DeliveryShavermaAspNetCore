12f052be22f73d8364bdd5b7d8c37fa2cd88d590
KevinBacon <turnipsh@student.21-school.ru>
2024-06-19 14:43:31 +0300

    commit tests

---
+++ UserService/UserService.Application/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace UserService.Application;
+
+public class Class1
+{
+}
---
+++ UserService/UserService.UnitTests/UserRepoTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using UserService.Core;
+using UserService.Core.abstractions;
+using UserService.DataAccess;
+using UserService.DataAccess.Extentions;
+using UserService.DataAccess.Repositories;
+using UserService.UnitTests.Fixtures;
+using UserService.UnitTests.Utils;
+
+namespace UserService.UnitTests;
+
+public class UserRepoTests
+{
+    private IUserRepository _userRepository;
+
+    [SetUp]
+    public void Setup()
+    {
+        _userRepository = new UserRepository(DbContextFixture.Context);
+    }
+
+    [Test]
+    public async Task AddAndGet()
+    {
+        var newUser = MyUser.Create(Guid.NewGuid(), ["address1", "address2", "address3"], "comment").myUser;
+
+        var result = await _userRepository.Save(newUser!);
+        var user = await _userRepository.Get(result);
+
+        Assert.That(user.IsEqual(newUser), Is.True);
+        Assert.That(result, Is.EqualTo(newUser.UserId));
+    }
+
+
+    [Test]
+    public async Task AddGetUpdateGet()
+    {
+        var newUser = MyUser.Create(Guid.NewGuid(), ["address1", "address2", "address3"], "comment").myUser;
+
+        var userId = await _userRepository.Save(newUser!);
+        var userToUpdate = await _userRepository.Get(newUser.UserId);
+        userToUpdate!.Addresses.Add("ул бебринска");
+        await _userRepository.Update(userToUpdate);
+        var updatedUser = await _userRepository.Get(userToUpdate.UserId);
+
+        Assert.That(updatedUser.IsEqual(userToUpdate), Is.True);
+    }
+}
