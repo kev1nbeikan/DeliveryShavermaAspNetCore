52dd2f7bc079453beca444133dd79232025a20dc
airat <airat.hb@mail.ru>
2024-06-19 23:46:34 +0300

    add storeAddress

--- OrderService/OrderService.Api/Controllers/CourierController.cs
+++ OrderService/OrderService.Api/Controllers/CourierController.cs
@@ -11,34 +11,34 @@ namespace OrderService.Api.Controllers;
 public class CourierController(IOrderApplicationService orderApplicationService) : ControllerBase
 {
     private readonly IOrderApplicationService _orderApplicationService = orderApplicationService;
-    
+
     [HttpGet("current")]
     public async Task<ActionResult<List<CourierGetCurrent>>> GetCurrent(int filter)
     {
         var userId = User.UserId();
         var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
-        
+
         var orders = await _orderApplicationService.GetCurrentOrders(role, userId);
         if (orders.Count == 0)
             return NoContent();
-        var response = orders.Select(b => 
-            new CourierGetCurrent(b.Id, b.StoreId, b.Status, b.Basket, b.Comment,
+        var response = orders.Select(b =>
+            new CourierGetCurrent(b.Id, b.Status, b.Basket, b.Comment, b.StoreAddress,
                 b.ClientAddress, b.ClientNumber, b.DeliveryTime));
         return Ok(response);
     }
-    
+
     [HttpGet("last")]
     public async Task<ActionResult<List<CourierGetLast>>> GetLast(int filter)
     {
         var userId = User.UserId();
         var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
-        
+
         var orders = await _orderApplicationService.GetLastOrders(role, userId);
         if (orders.Count == 0)
             return NoContent();
-        var response = orders.Select(b => 
-            new CourierGetLast(b.Id, b.StoreId, b.Basket, b.Comment,
-                b.DeliveryTime, b.OrderDate, b.CookingDate, b.DeliveryDate));
+        var response = orders.Select(b =>
+            new CourierGetLast(b.Id, b.StoreId, b.Basket, b.Comment, b.DeliveryTime,
+                b.OrderDate, b.CookingDate, b.DeliveryDate));
         return Ok(response);
     }
 }
