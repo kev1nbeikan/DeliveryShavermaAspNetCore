a86d2929ec3850c7d487cb40fffa264e082a4122
airat <airat.hb@mail.ru>
2024-06-20 13:20:59 +0300

    add change status, last, get new by date method

--- OrderService/OrderService.Api/Controllers/CourierController.cs
+++ OrderService/OrderService.Api/Controllers/CourierController.cs
@@ -11,9 +11,9 @@ namespace OrderService.Api.Controllers;
 public class CourierController(IOrderApplicationService orderApplicationService) : ControllerBase
 {
     private readonly IOrderApplicationService _orderApplicationService = orderApplicationService;
-
+    [HttpGet]
     [HttpGet("current")]
-    public async Task<ActionResult<List<CourierGetCurrent>>> GetCurrent(int filter)
+    public async Task<ActionResult<List<CourierGetCurrent>>> GetCurrent()
     {
         var userId = User.UserId();
         var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
@@ -28,7 +28,7 @@ public class CourierController(IOrderApplicationService orderApplicationService)
     }
 
     [HttpGet("last")]
-    public async Task<ActionResult<List<CourierGetLast>>> GetLast(int filter)
+    public async Task<ActionResult<List<CourierGetLast>>> GetLast()
     {
         var userId = User.UserId();
         var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
@@ -40,5 +40,50 @@ public class CourierController(IOrderApplicationService orderApplicationService)
             new CourierGetLast(b.Id, b.StoreId, b.Basket, b.Comment, b.DeliveryTime,
                 b.OrderDate, b.CookingDate, b.DeliveryDate));
         return Ok(response);
+    }
+    
+    [HttpGet("latest_order")]
+    public async Task<ActionResult<CourierGetCurrent>> GetLatestOrder()
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+
+        var order = await _orderApplicationService.GetNewestOrder(role, userId);
+        var response = new CourierGetCurrent(order.Id, order.Status, order.Basket, order.Comment,
+            order.StoreAddress, order.ClientAddress, order.ClientNumber, order.DeliveryTime);
+        return Ok(response);
+    }
+    
+    [HttpGet("get_new_orders/{lastOrderDate:Datetime}")]
+    public async Task<ActionResult<List<CourierGetCurrent>>> GetNewOrderByDate(DateTime lastOrderDate)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+
+        var orders = await _orderApplicationService.GetNewOrdersByDate(role, userId, lastOrderDate);
+        var response = orders.Select(b =>
+            new CourierGetCurrent(b.Id, b.Status, b.Basket, b.Comment, b.StoreAddress,
+                b.ClientAddress, b.ClientNumber, b.DeliveryTime));
+        return Ok(response);
+    }
+    
+    [HttpPut("{orderId:Guid}/delivering")] 
+    public async Task<ActionResult> ChangeStatusDelivering(Guid orderId)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+        
+        await _orderApplicationService.ChangeStatusActive(role, Domain.Models.Code.StatusCode.Delivering, userId, orderId);
+        return Ok();
+    }
+
+    [HttpPut("{orderId:Guid}/waitingClient")] 
+    public async Task<ActionResult> ChangeStatusWaitingClient(Guid orderId)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+        
+        await _orderApplicationService.ChangeStatusActive(role, Domain.Models.Code.StatusCode.WaitingClient, userId, orderId);
+        return Ok();
     }
 }
--- OrderService/OrderService.Api/Controllers/StoreController.cs
+++ OrderService/OrderService.Api/Controllers/StoreController.cs
@@ -11,34 +11,69 @@ namespace OrderService.Api.Controllers;
 public class StoreController(IOrderApplicationService orderApplicationService) : ControllerBase
 {
     private readonly IOrderApplicationService _orderApplicationService = orderApplicationService;
-    
+    [HttpGet]
     [HttpGet("current")]
-    public async Task<ActionResult<List<StoreGetCurrent>>> GetCurrent(int filter)
+    public async Task<ActionResult<List<StoreGetCurrent>>> GetCurrent()
     {
         var userId = User.UserId();
         var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
-        
+
         var orders = await _orderApplicationService.GetCurrentOrders(role, userId);
         if (orders.Count == 0)
             return NoContent();
-        var response = orders.Select(b => 
+        var response = orders.Select(b =>
+            new StoreGetCurrent(b.Id, b.Status, b.Basket, b.Comment,
+                b.CourierNumber, b.CookingTime));
+        return Ok(response);
+    }
+
+    [HttpGet("last")]
+    public async Task<ActionResult<List<StoreGetLast>>> GetLast()
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+
+        var orders = await _orderApplicationService.GetLastOrders(role, userId);
+        if (orders.Count == 0)
+            return NoContent();
+        var response = orders.Select(b =>
+            new StoreGetLast(b.Id, b.Basket, b.Comment, b.CookingTime,
+                b.OrderDate, b.CookingDate));
+        return Ok(response);
+    }
+
+    [HttpGet("latest_order")]
+    public async Task<ActionResult<StoreGetCurrent>> GetLatestOrder()
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+
+        var order = await _orderApplicationService.GetNewestOrder(role, userId);
+        var response = new StoreGetCurrent(order.Id, order.Status, order.Basket, order.Comment,
+            order.ClientNumber, order.DeliveryTime);
+        return Ok(response);
+    }
+    
+    [HttpGet("get_new_orders/{lastOrderDate:Datetime}")]
+    public async Task<ActionResult<List<StoreGetCurrent>>> GetNewOrderByDate(DateTime lastOrderDate)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+
+        var orders = await _orderApplicationService.GetNewOrdersByDate(role, userId, lastOrderDate);
+        var response = orders.Select(b =>
             new StoreGetCurrent(b.Id, b.Status, b.Basket, b.Comment,
                 b.CourierNumber, b.CookingTime));
         return Ok(response);
     }
     
-    [HttpGet("last")]
-    public async Task<ActionResult<List<StoreGetLast>>> GetLast(int filter)
+    [HttpPut("{orderId:Guid}/waitingCourier")] 
+    public async Task<ActionResult> ChangeStatusWaitingCourier(Guid orderId)
     {
         var userId = User.UserId();
         var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
         
-        var orders = await _orderApplicationService.GetLastOrders(role, userId);
-        if (orders.Count == 0)
-            return NoContent();
-        var response = orders.Select(b => 
-            new StoreGetLast(b.Id, b.Basket, b.Comment, b.CookingTime,
-                b.OrderDate, b.CookingDate));
-        return Ok(response);
+        await _orderApplicationService.ChangeStatusActive(role, Domain.Models.Code.StatusCode.WaitingCourier, userId, orderId);
+        return Ok();
     }
 }
