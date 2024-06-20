845cc9fa9673ca199eb3e723666fe191c3d73228
airat <airat.hb@mail.ru>
2024-06-20 13:22:05 +0300

    create creteOrder, getOrderStatus, changeStatusCanceled method

--- OrderService/OrderService.Api/Controllers/OrderController.cs
+++ OrderService/OrderService.Api/Controllers/OrderController.cs
@@ -1,6 +1,77 @@
-﻿namespace OrderService.Api.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Api.Contracts.Order;
+using OrderService.Api.Extensions;
+using OrderService.Domain.Abstractions;
+using OrderService.Domain.Models;
+using OrderService.Domain.Models.Code;
 
-public class OrderController
+namespace OrderService.Api.Controllers;
+
+[ApiController]
+[Route("orders")]
+public class OrderController(IOrderApplicationService orderApplicationService) : ControllerBase
 {
+    private readonly IOrderApplicationService _orderApplicationService = orderApplicationService;
+    [HttpGet("{orderId:Guid}")]
+    public async Task<ActionResult<int>> GetStatus(Guid orderId)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+
+        var status = await _orderApplicationService.GetStatus(role, userId, orderId);
+        return Ok((int)status);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateOrder([FromBody]OrderCreateRequest request)
+    {
+        // var userId = User.UserId();
+        // var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+
+        var (order, errorString) = CurrentOrder.Create(
+            request.Id,
+            request.ClientId,
+            request.CourierId,
+            request.StoreId,
+            request.Basket,
+            request.Price,
+            request.Comment,
+            request.StoreAddress,
+            request.ClientAddress,
+            request.CourierNumber,
+            request.ClientNumber,
+            request.CookingTime,
+            request.DeliveryTime,
+            DateTime.UtcNow,
+            null,
+            null,
+            request.Cheque,
+            Domain.Models.Code.StatusCode.Cooking);
+
+        if (!string.IsNullOrEmpty(errorString))
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Title = "Error when creating an order",
+                Detail = errorString,
+            };
+            return BadRequest(problemDetails);
+        }
+        await _orderApplicationService.CreateCurrentOrder(order);
+        return Ok();
+    }
     
+    [HttpPut("{orderId:Guid}/canceled")]
+    public async Task<ActionResult> ChangeStatusCanceled(Guid orderId, [FromBody] string reasonOfCanceled)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+        
+        await _orderApplicationService.ChangeStatusCanceled(role, userId, orderId, reasonOfCanceled);
+        return Ok();
+    }
 }
+
+
+
+
