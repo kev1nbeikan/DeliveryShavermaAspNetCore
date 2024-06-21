bc6dcc25d56134ebe0aa1447029ae9cf8d81cb2b
airat <airat.hb@mail.ru>
2024-06-21 14:55:49 +0300

    add canceled status, delete get new by date, refactor latest->active

--- OrderService/OrderService.Api/Controllers/CourierController.cs
+++ OrderService/OrderService.Api/Controllers/CourierController.cs
@@ -42,30 +42,18 @@ public class CourierController(IOrderApplicationService orderApplicationService)
         return Ok(response);
     }
     
-    [HttpGet("latest_order")]
-    public async Task<ActionResult<CourierGetCurrent>> GetLatestOrder()
+    [HttpGet("active")]
+    public async Task<ActionResult<CourierGetCurrent>> GetOldestActive()
     {
         var userId = User.UserId();
         var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
 
-        var order = await _orderApplicationService.GetNewestOrder(role, userId);
+        var order = await _orderApplicationService.GetOldestActive(role, userId);
         var response = new CourierGetCurrent(order.Id, order.Status, order.Basket, order.Comment,
             order.StoreAddress, order.ClientAddress, order.ClientNumber, order.DeliveryTime);
         return Ok(response);
     }
     
-    [HttpGet("get_new_orders/{lastOrderDate:Datetime}")]
-    public async Task<ActionResult<List<CourierGetCurrent>>> GetNewOrderByDate(DateTime lastOrderDate)
-    {
-        var userId = User.UserId();
-        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
-
-        var orders = await _orderApplicationService.GetNewOrdersByDate(role, userId, lastOrderDate);
-        var response = orders.Select(b =>
-            new CourierGetCurrent(b.Id, b.Status, b.Basket, b.Comment, b.StoreAddress,
-                b.ClientAddress, b.ClientNumber, b.DeliveryTime));
-        return Ok(response);
-    }
     
     [HttpPut("{orderId:Guid}/delivering")] 
     public async Task<ActionResult> ChangeStatusDelivering(Guid orderId)
@@ -84,6 +72,16 @@ public class CourierController(IOrderApplicationService orderApplicationService)
         var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
         
         await _orderApplicationService.ChangeStatusActive(role, Domain.Models.Code.StatusCode.WaitingClient, userId, orderId);
+        return Ok();
+    }
+    
+    [HttpPut("{orderId:Guid}/canceled")]
+    public async Task<ActionResult> ChangeStatusCanceled(Guid orderId, [FromBody] string reasonOfCanceled)
+    {
+        var userId = User.UserId();
+        var role = (RoleCode)Enum.Parse(typeof(RoleCode), User.Role());
+        
+        await _orderApplicationService.ChangeStatusCanceled(role, userId, orderId, reasonOfCanceled);
         return Ok();
     }
 }
