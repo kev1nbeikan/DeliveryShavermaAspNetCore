4697d6ee6f9e4c81135f63aa93f0468a8653bb94
airat <airat.hb@mail.ru>
2024-06-23 17:56:45 +0300

    create new class for basket

---
+++ OrderService/OrderService.Domain/Models/BasketItem.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderService.Domain.Models;
+
+public abstract class BasketItem
+{
+    public Guid ProductId { get; set; }
+    public int Amount { get; set; }
+    public int Price { get; set; }
+}
