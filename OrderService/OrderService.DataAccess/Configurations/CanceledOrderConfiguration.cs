4a3882819e5ac685f2cb5ee2ff96d261ce6f00a8
airat <airat.hb@mail.ru>
2024-06-24 20:47:43 +0300

    delete constraint

--- OrderService/OrderService.DataAccess/Configurations/CanceledOrderConfiguration.cs
+++ OrderService/OrderService.DataAccess/Configurations/CanceledOrderConfiguration.cs
@@ -16,10 +16,6 @@ namespace OrderService.DataAccess.Configurations
             builder.Property(b => b.LastStatus)
                 .IsRequired();
 
-            builder.ToTable(t =>
-                t.HasCheckConstraint("CK_CanceledOrder_LastStatus",
-                    $"\"LastStatus\" >= 1 AND \"LastStatus\" < {(int)StatusCode.Accepted}"));
-
             builder.Property(b => b.ReasonOfCanceled)
                 .HasMaxLength(BaseOrder.MaxCommentLength);
         }
