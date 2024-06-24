868a110f3665a71fc54922c26ec4ac5d7dc48a95
airat <airat.hb@mail.ru>
2024-06-24 22:07:32 +0300

    add new migration canceledOrder_add_colum_CanceledDate

--- OrderService/OrderService.DataAccess/Migrations/20240624190550_canceledOrder_add_colum_canceledDate.Designer.cs
+++ OrderService/OrderService.DataAccess/Migrations/20240624190550_canceledOrder_add_colum_canceledDate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OrderService.DataAccess;
@@ -11,9 +12,11 @@ using OrderService.DataAccess;
 namespace OrderService.DataAccess.Migrations
 {
     [DbContext(typeof(OrderServiceDbContext))]
-    partial class OrderServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240624190550_canceledOrder_add_colum_canceledDate")]
+    partial class canceledOrder_add_colum_canceledDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
