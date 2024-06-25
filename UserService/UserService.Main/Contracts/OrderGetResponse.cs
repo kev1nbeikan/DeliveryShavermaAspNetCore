db2d4cf3b322f8a29566a0ad859239e984cf4040
airat <airat.hb@mail.ru>
2024-06-25 16:41:46 +0300

    add dto for order method on controller

---
+++ UserService/UserService.Main/Contracts/OrderGetResponse.cs
@@ -0,0 +1,16 @@
+﻿using UserService.Core;
+
+namespace UserService.Main.Contracts;
+
+public class OrderGetResponse
+{
+    public Guid Id { get; set; }
+    public int Status { get; set; }
+    public List<BasketItem> Basket { get; set; } = [];
+    int Price { get; set; }
+    string Comment { get; set; } = "";
+    string ClientAddress { get; set; } = "";
+    string CourierNumber { get; set; } = "";
+    string ClientNumber { get; set; } = "";
+    string Cheque { get; set; } = "";
+}
