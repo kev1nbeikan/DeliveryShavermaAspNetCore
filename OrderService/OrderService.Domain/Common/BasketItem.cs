8312c462d4c56017c6a87e8232aed5494e9e2ebc
airat <airat.hb@mail.ru>
2024-06-27 01:26:22 +0300

    add field name

--- OrderService/OrderService.Domain/Common/BasketItem.cs
+++ OrderService/OrderService.Domain/Common/BasketItem.cs
@@ -3,6 +3,7 @@
 public struct BasketItem
 {
     public Guid ProductId { get; set; }
+    public string Name { get; set; }
     public int Amount { get; set; }
     public int Price { get; set; }
 }
