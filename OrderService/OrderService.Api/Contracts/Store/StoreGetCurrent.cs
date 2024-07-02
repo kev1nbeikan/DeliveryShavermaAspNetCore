d86600d15f9edaedb64d991dc96d6e3b3f2c68c1
airat <airat.hb@mail.ru>
2024-07-02 19:38:17 +0300

    добавил айди потом удалить

--- OrderService/OrderService.Api/Contracts/Store/StoreGetCurrent.cs
+++ OrderService/OrderService.Api/Contracts/Store/StoreGetCurrent.cs
@@ -5,6 +5,7 @@ namespace OrderService.Api.Contracts.Store;
 
 public record StoreGetCurrent(
     Guid Id,
+    Guid ClientId, // удалить потом
     StatusCode Status,
     List<BasketItem> Basket,
     string Comment,
